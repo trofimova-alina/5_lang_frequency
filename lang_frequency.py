4a59dad82b53c374c4d2b30f1177220f94ff62eb
Alina <trofimova.alinka@yandex.ru>
2016-09-13 18:22:34 +0300

    Words frequency

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,26 @@
+import re
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as raw_content:
+        data = raw_content.read()
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    wordlist = re.findall(r'\w+', text)
+    dict_of_words = {}
+    for current_word in wordlist:
+        if current_word in dict_of_words.keys():
+            continue
+        else:
+            dict_of_words[current_word] = wordlist.count(current_word)
+    return dict_of_words
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('test.txt')
+    list_of_words = list(get_most_frequent_words(data).items())
+    list_of_words.sort(key=lambda item: item[1], reverse=True)
+    for i in range(0, 10):
+        print(list_of_words[i])
