9c07a1e9d707a12ceabbb91236c474e59544d6d3
Alina <trofimova.alinka@yandex.ru>
2016-09-27 00:09:20 +0300

    Fixed bugs Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 5_lang_frequency
+
+1. Put text file test.txt to 5_lang_frequency folder
+2. Run lang_frequency.py
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,4 +1,7 @@
 import re
+from collections import Counter
+
+WORDS_COUNT = 10
 
 
 def load_data(filepath):
@@ -8,19 +11,10 @@ def load_data(filepath):
 
 
 def get_most_frequent_words(text):
-    wordlist = re.findall(r'\w+', text)
-    dict_of_words = {}
-    for current_word in wordlist:
-        if current_word in dict_of_words.keys():
-            continue
-        else:
-            dict_of_words[current_word] = wordlist.count(current_word)
-    return dict_of_words
+    wordlist = re.findall(r'\w+', text.lower())
+    return Counter(wordlist).most_common(WORDS_COUNT)
 
 
 if __name__ == '__main__':
     data = load_data('test.txt')
-    list_of_words = list(get_most_frequent_words(data).items())
-    list_of_words.sort(key=lambda item: item[1], reverse=True)
-    for i in range(0, 10):
-        print(list_of_words[i])
+    print(get_most_frequent_words(data))
